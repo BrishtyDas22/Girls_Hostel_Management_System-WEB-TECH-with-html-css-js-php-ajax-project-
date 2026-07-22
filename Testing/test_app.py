ad2c4bb92743d9bbc6f9faef1720bf5ad309e571
pkhunter47 <protikbiswas3099@gmail.com>
2026-07-23 02:25:58 +0600

    6 pages testing

--- Testing/test_app.py
+++ Testing/test_app.py
@@ -7,6 +7,9 @@ from selenium.webdriver.support.ui import Select
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 BASE_URL = "http://localhost/Hostel_Management/USER/VIEW/frontpage1.php"
+REGISTRATION_URL = "http://localhost/Hostel_Management/USER/VIEW/registration1.php"
+LOGIN_URL = "http://localhost/Hostel_Management/USER/VIEW/Login.php"
+FORGET_PASS_URL = "http://localhost/Hostel_Management/USER/VIEW/forget_password.php"
 
 @pytest.fixture(scope="module")
 def driver():
@@ -85,7 +88,6 @@ def test_navigation_buttons(driver):
         time.sleep(1)
     except:
         assert "admin" in driver.current_url.lower()
-REGISTRATION_URL = "http://localhost/Hostel_Management/USER/VIEW/registration1.php"
 
 def test_registration_form_submission(driver):
     driver.get(REGISTRATION_URL)
@@ -125,8 +127,6 @@ def test_registration_form_submission(driver):
 
     assert "login" in driver.current_url.lower() or "validation" in driver.current_url.lower() or driver.page_source
 
-
-LOGIN_URL = "http://localhost/Hostel_Management/USER/VIEW/Login.php"
 def test_login_form_submission(driver):
     driver.get(LOGIN_URL)
     time.sleep(1)
@@ -134,13 +134,13 @@ def test_login_form_submission(driver):
     inputs = driver.find_elements(By.TAG_NAME, "input")
 
     inputs[0].clear()
-    inputs[0].send_keys("Protik Biswas")
+    inputs[0].send_keys("Test User")
 
     inputs[1].clear()
-    inputs[1].send_keys("protikbiswas2088@gmail.com")
+    inputs[1].send_keys("testuser_1784739815@example.com")
 
     inputs[2].clear()
-    inputs[2].send_keys("111111")
+    inputs[2].send_keys("Pass1234!")
 
     login_btn = driver.find_element(By.XPATH, "//button[contains(text(), 'Login')] | //input[@type='submit' or @value='Login']")
     login_btn.click()
@@ -155,3 +155,113 @@ def test_login_form_submission(driver):
         pass
 
     assert driver.current_url != LOGIN_URL or driver.page_source
+
+def test_forget_password_flow(driver):
+    driver.get(LOGIN_URL)
+    time.sleep(1)
+
+    forget_pwd_link = driver.find_element(By.XPATH, "//*[contains(text(), 'Forget password?')]")
+    forget_pwd_link.click()
+    time.sleep(1)
+
+    assert "forget_password.php" in driver.current_url.lower()
+
+    inputs = driver.find_elements(By.TAG_NAME, "input")
+
+    inputs[0].clear()
+    inputs[0].send_keys("Test User")
+
+    inputs[1].clear()
+    inputs[1].send_keys("NewPass123!")
+
+    inputs[2].clear()
+    inputs[2].send_keys("NewPass123!")
+
+    reset_btn = driver.find_element(By.XPATH, "//button[contains(text(), 'Reset Password')] | //input[@type='submit' or @value='Reset Password']")
+    reset_btn.click()
+    time.sleep(2)
+
+    try:
+        alert = driver.switch_to.alert
+        alert.accept()
+        time.sleep(1)
+    except:
+        pass
+
+    driver.get(FORGET_PASS_URL)
+    time.sleep(1)
+
+    back_to_login_link = driver.find_element(By.XPATH, "//*[contains(text(), 'Go back to Login')]")
+    back_to_login_link.click()
+    time.sleep(1)
+
+    assert "login.php" in driver.current_url.lower()
+
+def test_room_booking_and_payment_flow(driver):
+    driver.get(LOGIN_URL)
+    time.sleep(1)
+
+    inputs = driver.find_elements(By.TAG_NAME, "input")
+    inputs[0].clear()
+    inputs[0].send_keys("Protik Biswas")
+
+    inputs[1].clear()
+    inputs[1].send_keys("protikbiswas2088@gmail.com")
+
+    inputs[2].clear()
+    inputs[2].send_keys("111111")
+
+    login_btn = driver.find_element(By.XPATH, "//button[contains(text(), 'Login')] | //input[@type='submit' or @value='Login']")
+    login_btn.click()
+    time.sleep(2)
+
+    try:
+        alert = driver.switch_to.alert
+        alert.accept()
+        time.sleep(1)
+    except:
+        pass
+
+    assert "afterlogin.php" in driver.current_url.lower()
+
+    book_now_btn = driver.find_element(By.XPATH, "//button[contains(text(), 'Book Now!')] | //a[contains(text(), 'Book Now!')] | //*[contains(text(), 'Book Now!')]")
+    book_now_btn.click()
+    time.sleep(2)
+
+    assert "paymentmethod.php" in driver.current_url.lower()
+
+    bkash_radio = driver.find_element(By.XPATH, "//input[@type='radio' and (@value='Bkash' or @value='bkash')] | //*[contains(text(), 'Bkash')]/preceding-sibling::input[1]")
+    bkash_radio.click()
+
+    all_inputs = driver.find_elements(By.XPATH, "//input[@type='text' or not(@type)]")
+
+    editable_inputs = [inp for inp in all_inputs if not inp.get_attribute("readonly")]
+
+    if len(editable_inputs) >= 2:
+        
+        editable_inputs[0].clear()
+        editable_inputs[0].send_keys("01615663862")
+
+       
+        editable_inputs[1].clear()
+        editable_inputs[1].send_keys("22222222")
+    else:
+        for inp in all_inputs:
+            try:
+                if inp.is_enabled() and inp.get_attribute("type") not in ["radio", "submit", "button", "hidden"]:
+                    inp.send_keys("22222222")
+            except:
+                pass
+
+    confirm_btn = driver.find_element(By.XPATH, "//button[contains(text(), 'Confirm Payment')] | //input[@type='submit' or @value='Confirm Payment']")
+    confirm_btn.click()
+    time.sleep(2)
+
+    try:
+        alert = driver.switch_to.alert
+        alert.accept()
+        time.sleep(1)
+    except:
+        pass
+
+    assert driver.current_url != "http://localhost/Hostel_Management/USER/VIEW/paymentmethod.php?room=102" or driver.page_source
